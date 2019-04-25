f7e099d5c313de635d5f4ad2ab40de79afbb1277
Res <39837674+prestonwhitehurst@users.noreply.github.com>
2019-04-25 19:19:06 -0400

    Add files via upload

--- src/me/prestonwhitehurst/fileviewer/FileViewerController.java
+++ src/me/prestonwhitehurst/fileviewer/FileViewerController.java
@@ -1,18 +1,28 @@
 package me.prestonwhitehurst.fileviewer;
 
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TableRow;
+import javafx.scene.layout.VBox;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import java.io.File;
 
 public class FileViewerController {
     private final FileViewerModel model;
     private final FileViewerView view;
+    private ObservableList<FileWrapper> data;
 
     public FileViewerController(FileViewerModel model, FileViewerView view) {
         this.model = model;
         this.view = view;
-        setOnActionBackButton(view.getBackButton());
-        updateFilesFlowPane(view.getFilesFlowPane());
+        initialiseTable(view.getFilesTable());
+        initialiseButton(view.getBackButton(), view.getFilesTable());
+    }
+
+    public VBox getView() {
+        return view.getRoot();
     }
 
     public void updateCurrentDirectory(File file) {
@@ -23,44 +33,41 @@ public class FileViewerController {
         model.removeCurrentDirectory();
     }
 
-    public File[] getFiles() {
+    public ObservableList<FileWrapper> getFiles() {
         return model.getCurrentDirectoryFiles();
     }
 
-    public void setOnActionBackButton(Button backButton) {
-        backButton.setOnAction(e -> {
-            removeCurrentDirectory();
-            updateFilesFlowPane(view.getFilesFlowPane());
+    public void initialiseTable(TableView<FileWrapper> filesTable) {
+        TableColumn nameCol = new TableColumn("Name");
+        TableColumn sizeCol = new TableColumn("Size");
+        TableColumn dateLastModifiedCol = new TableColumn("Last Modified");
+        nameCol.setMinWidth(250);
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        sizeCol.setMinWidth(125);
+        sizeCol.setCellValueFactory(new PropertyValueFactory<>("size"));
+        dateLastModifiedCol.setMinWidth(200);
+        dateLastModifiedCol.setCellValueFactory(new PropertyValueFactory<>("dateLastModified"));
+        data = getFiles();
+        filesTable.setItems(data);
+        filesTable.setRowFactory(tableView -> {
+            TableRow<FileWrapper> row = new TableRow<>();
+            row.setOnMouseClicked(e -> {
+                if(e.getClickCount() == 2 && (!row.isEmpty())) {
+                    updateCurrentDirectory(row.getItem().getFile());
+                    data = getFiles();
+                    filesTable.setItems(data);
+                }
+            });
+            return row;
         });
+        filesTable.getColumns().addAll(nameCol, sizeCol, dateLastModifiedCol);
     }
 
-    public void updateFilesFlowPane(FlowPane filesFlowPane) {
-        if(filesFlowPane.getChildren() != null) {
-            filesFlowPane.getChildren().clear();
-            File[] f = getFiles();
-            for(final File file : f) {
-                Button fileButton = new Button(file.getName());
-                fileButton.setOnMouseClicked(e -> {
-                    if(e.getClickCount() == 2) {
-                        updateCurrentDirectory(file.getAbsoluteFile());
-                        updateFilesFlowPane(filesFlowPane);
-                    }
-                });
-                filesFlowPane.getChildren().add(fileButton);
-            }
-        }
-        else {
-            File[] f = getFiles();
-            for(final File file : f) {
-                Button fileButton = new Button(file.getName());
-                fileButton.setOnMouseClicked(e -> {
-                    if(e.getClickCount() == 2) {
-                        updateCurrentDirectory(file.getAbsoluteFile());
-                        updateFilesFlowPane(filesFlowPane);
-                    }
-                });
-                filesFlowPane.getChildren().add(fileButton);
-            }
-        }
+    public void initialiseButton(Button backButton, TableView<FileWrapper> filesTable) {
+        backButton.setOnAction(e -> {
+            removeCurrentDirectory();
+            data = getFiles();
+            filesTable.setItems(data);
+        });
     }
 }
--- src/me/prestonwhitehurst/fileviewer/FileViewerModel.java
+++ src/me/prestonwhitehurst/fileviewer/FileViewerModel.java
@@ -1,7 +1,11 @@
 package me.prestonwhitehurst.fileviewer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 import java.io.File;
+import java.util.Arrays;
 
 public class FileViewerModel {
     private final ArrayList<File> directoryArrayList;
@@ -36,7 +40,14 @@ public class FileViewerModel {
         }
     }
 
-    public File[] getCurrentDirectoryFiles() {
-        return getCurrentDirectory().listFiles();
+    public ObservableList<FileWrapper> getCurrentDirectoryFiles() {
+        ArrayList<File> f = new ArrayList<>(Arrays.asList(getCurrentDirectory().listFiles()));
+        ArrayList<FileWrapper> fw = new ArrayList<>();
+
+        for(File file : f) {
+            fw.add(new FileWrapper(file));
+        }
+
+        return FXCollections.observableList(fw);
     }
 }
